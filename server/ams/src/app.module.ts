68d0448cc5096f0eb559438d658e8c0d3adf0e48
Darshan B.S <darshan.b.s@tadigital.com>
2021-02-28 11:45:57 +0530

    stable project setup

--- server/ams/src/app.module.ts
+++ server/ams/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeesModule } from './employees/employees.module';
+import { QueryprocessorModule } from './queryprocessor/queryprocessor.module';
+import { CrudfactoryModule } from './crudfactory/crudfactory.module';
+import { DepartmentsModule } from './departments/departments.module';
+import { HolidaysModule } from './holidays/holidays.module';
+import { LeavesModule } from './leaves/leaves.module';
+import { AttendancesModule } from './attendances/attendances.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeesModule, QueryprocessorModule, CrudfactoryModule, DepartmentsModule, HolidaysModule, LeavesModule, AttendancesModule, ProjectsModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
